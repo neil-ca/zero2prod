8753aba8b68b953f88a1a6cff8009ff48606fc35
neil-ca <ulisescamps9@gmail.com>
2023-04-14 00:48:42 -0600

    Anatomy on an actix-web app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, HttpServer, App };
+
+async fn greet() -> String {
+    format!("Welcome !")
 }
+
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
+
