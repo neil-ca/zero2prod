063484cb02d7899a7d1304f7131bf6c423062d4b
neil-ca <ulisescamps9@gmail.com>
2023-04-16 02:47:12 -0600

    Testing points

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
-use actix_web::{web, HttpServer, App };
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, HttpRequest};
 
-async fn greet() -> String {
-    format!("Welcome !")
+//async fn greet() -> String {
+    //format!("Welcome !")
+//}
+
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("Hello!")
 }
 
-
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/", web::get().to(greet))
+            .route("/", web::get().to(index))
+            .route("/health_check", web::get().to(health_check))
+            .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
     .await
 }
-
