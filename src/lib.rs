8b7b4d34b751aa714a24a591e930c1661a1eab8c
neil-ca <ulisescamps9@gmail.com>
2023-04-21 15:54:41 -0600

    Creating postgres instance, migrations with sqlx-cli

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,35 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-//pub async fn run() -> std::io::Result<()> {
-//HttpServer::new(|| {
-//App::new()
-//.route("/health_check", web::get().to(health_check))
-//.route("/", web::get().to(index))
-//.route("/{name}", web::get().to(greet))
-//})
-//.bind("127.0.0.1:8000")?
-//.run()
-//.await
-//}
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String
+}
 
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
+//pub enum Payload<S = Pin<Box<dyn Stream<Item = Result<Bytes, PayloadError>> + 'static, Global>>> {
+    //None,
+    //H1 {
+        //payload: Payload,
+    //},
+    //H2 {
+        //payload: Payload,
+    //},
+    //Stream {
+        //payload: S,
+    //},
+//}
