55ebaae4a0e4d53a3d528b42389b151f9b173b9b
neil-ca <ulisescamps9@gmail.com>
2023-04-28 18:02:27 -0600

    Instrumenting POST /subscriptions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::net::TcpListener;
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are fallingback to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,17 @@
-use std::net::TcpListener;
+use crate::routes::{health_check, subscribe};
+use actix_web::middleware::Logger;
+use actix_web::web::Data;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
-use crate::routes::{health_check, subscribe};
+use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the connections in a smart poiner
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
