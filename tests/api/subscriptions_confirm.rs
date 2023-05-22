347d8060e530f11e4cb68f64de89812e0e281880
neil-ca <ulisescamps9@gmail.com>
2023-05-21 23:52:10 -0600

    Generating Subscription Tokens, Red test

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -31,3 +31,34 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
         .unwrap();
     assert_eq!(response.status().as_u16(), 200)
 }
+
+#[tokio::test] 
+async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=neil%20ca&email=neil%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscribtion(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(&email_request);
+
+    reqwest::get(confirmation_links.html)
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "neil@gmail.com");
+    assert_eq!(saved.name, "neil ca");
+    assert_eq!(saved.status, "confirmed");
+}
