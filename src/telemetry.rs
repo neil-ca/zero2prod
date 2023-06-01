0c9be890dd29f14bbc04d534a6fd06f6b7dd3570
neil-ca <ulisescamps9@gmail.com>
2023-05-31 19:02:42 -0600

    Implementing authentication and password hashing

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -3,6 +3,7 @@ use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry, fmt::MakeWriter};
+use actix_web::rt::task::JoinHandle;
 // Compose multiple layers into a `tracing`s subscriber.
 //
 // # Implementation Notes
@@ -35,4 +36,13 @@ pub fn get_subscriber<Sink>(name: String, env_filter: String, sink: Sink) -> imp
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
     LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
+pub fn spawn_blocking_with_tracing<F, R>(f: F) -> JoinHandle<R>
+where
+    F: FnOnce() -> R + Send + 'static,
+    R: Send + 'static
+{
+    let current_span = tracing::Span::current();
+    actix_web::rt::task::spawn_blocking(move || current_span.in_scope(f))
 }
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,3 +1,5 @@
+use argon2::{Argon2, PasswordHasher, Algorithm, Version, Params};
+use argon2::password_hash::SaltString;
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
@@ -27,6 +29,7 @@ pub struct TestApp {
     pub port: u16,
     pub db_pool: PgPool,
     pub email_server: MockServer,
+    test_user: TestUser
 }
 
 // Confirmation links embedded in the request to the email API
@@ -46,6 +49,13 @@ impl TestApp {
             .expect("Failed to execute reqwest")
     }
 
+    pub async fn test_user(&self) -> (String, String) {
+        let row = sqlx::query!("SELECT username, password_hash FROM users LIMIT 1")
+            .fetch_one(&self.db_pool)
+            .await
+            .expect("Failed to create test users");
+        (row.username, row.password_hash)
+    }
     // Extract the confirmation links embedded in the request to the email API
     pub fn get_confirmation_links(&self, email_request: &wiremock::Request) -> ConfirmationLinks {
         let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
@@ -70,9 +80,11 @@ impl TestApp {
     }
 
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
+        let (username, password) = self.test_user().await;
+
         reqwest::Client::new()
             .post(&format!("{}/newsletters", &self.address))
-            .basic_auth(Uuid::new_v4().to_string(), Some(Uuid::new_v4().to_string()))
+            .basic_auth(&self.test_user.username, Some(&self.test_user.password))
             .json(&body)
             .send()
             .await
@@ -106,12 +118,28 @@ pub async fn spawn_app() -> TestApp {
     let application_port = application.port();
     let _ = tokio::spawn(application.run_until_stopped());
 
-    TestApp {
+    let test_app = TestApp {
         address: format!("http://127.0.0.1:{}", application_port),
         port: application_port,
         db_pool: get_connection_pool(&configuration.database),
         email_server,
-    }
+        test_user: TestUser::generate()
+    };
+    test_app.test_user.store(&test_app.db_pool).await;
+    test_app
+}
+
+async fn add_test_user(pool: &PgPool) {
+    sqlx::query!(
+        "INSERT INTO users (user_id, username, password_hash)
+        VALUES ($1, $2, $3)",
+        Uuid::new_v4(),
+        Uuid::new_v4().to_string(),
+        Uuid::new_v4().to_string(),
+    )
+    .execute(pool)
+    .await
+    .expect("Failed to create test users");
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
@@ -133,4 +161,43 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .await
         .expect("Failed to migrate database");
     connection_pool
+}
+
+// User
+pub struct TestUser {
+    user_id: Uuid,
+    pub username: String,
+    pub password: String
+}
+
+impl TestUser {
+    pub fn generate() -> Self {
+        Self {
+            user_id: Uuid::new_v4(),
+            username: Uuid::new_v4().to_string(),
+            password: Uuid::new_v4().to_string()
+        }
+    }
+
+    async fn store(&self, pool: &PgPool) {
+        let salt = SaltString::generate(&mut rand::thread_rng());
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None).unwrap(),
+        )
+        .hash_password(self.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
+        sqlx::query!(
+            "INSERT INTO users (user_id, username, password_hash)
+            VALUES ($1, $2, $3)",
+            self.user_id,
+            self.username,
+            password_hash,
+            )
+            .execute(pool)
+            .await
+            .expect("Failed to store test user");
+    }
 }
